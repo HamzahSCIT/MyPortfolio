02eac814b2c57244f89a837cd01f7ec42def59a8
HamzahSCIT <hamzahsaeed@gmail.com>
2022-12-02 23:19:22 +0500

    added initial paragraph

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -1,11 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 
 // @ts-ignore
-import srcsetAvif from "~/assets/images/hero.jpg?w=300;600&avif&srcset";
+import srcsetAvif from "~/assets/images/me2.png?w=300;600&avif&srcset";
 // @ts-ignore
-import srcsetWebp from "~/assets/images/hero.jpg?w=300;600&webp&srcset";
+import srcsetWebp from "~/assets/images/me2.png?w=300;600&webp&srcset";
 // @ts-ignore
-import { src as placeholder } from "~/assets/images/hero.jpg?width=300&metadata";
+import { src as placeholder } from "~/assets/images/me2.png?width=300&metadata";
+import { IconDownload } from "../icons/IconDownload";
 
 export default component$(() => {
   return (
@@ -15,26 +16,14 @@ export default component$(() => {
           <div class="pb-0 md:py-0 lg:py-16 max-w-5xl mx-auto md:pr-16 flex items-center basis-[56%]">
             <div>
               <h1 class="text-5xl md:text-[3.50rem] font-bold leading-tighter tracking-tighter mb-4 font-heading">
-                Free template for <br class="hidden lg:block" />{" "}
-                <span class="hidden lg:inline">create a website using </span>{" "}
-                <span class="text-[#039de1]">Qwik</span> +{" "}
-                <span class="sm:whitespace-nowrap text-[#039de1]">
-                  Tailwind CSS
-                </span>
+                Welcome to my portfolio website!
               </h1>
               <div class="max-w-3xl mx-auto">
                 <p class="text-xl text-gray-600 mb-8 dark:text-slate-400">
-                  <span class="font-semibold underline decoration-wavy decoration-1 decoration-secondary-600 underline-offset-2">
-                    Qwind
-                  </span>{" "}
-                  is a production ready template to start your new website using{" "}
-                  <em>Qwik</em> + <em>Tailwind CSS</em>. It has been designed
-                  following Best Practices, SEO, Accessibility,{" "}
-                  <span class="inline md:hidden">...</span>
-                  <span class="hidden md:inline">
-                    Dark Mode, Great Page Speed, image optimization, sitemap
-                    generation and more.
-                  </span>
+                 I am a software engineer from Pakistan who is 
+                 <span class="italic text-purple-600 animate-pulse"> proficient in </span> and 
+                 <span class="italic text-purple-600 animate-pulse"> passionate about </span>
+                 frontend development.
                 </p>
                 <div class="max-w-xs sm:max-w-md flex flex-nowrap flex-col sm:flex-row gap-4 m-auto md:m-0 justify-center md:justify-start">
                   <div class="flex w-full sm:w-auto">
@@ -44,7 +33,8 @@ export default component$(() => {
                       target="_blank"
                       rel="noopener"
                     >
-                      Get template
+                    <IconDownload />
+                      Download Resume
                     </a>
                   </div>
                   <div class="flex w-full sm:w-auto">
